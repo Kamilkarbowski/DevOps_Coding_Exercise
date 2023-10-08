9460ec536ae33f83879eeabf63ac295c1211ec23
Kamilkarbowski <115906542+Kamilkarbowski@users.noreply.github.com>
2023-10-07 22:29:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
   secret_key = ""
 }
 
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = "my-eks-cluster"
 }
